050f9e31ed598e251786e37d57f36a2b4a1709fd
kangyunseok <kangyunseok0906@gmail.com>
2021-12-07 11:08:45 +0900

    Initial commit

---
+++ consumer.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import threading, time
+
+from kafka import KafkaAdminClient, KafkaConsumer, KafkaProducer
+from kafka.admin import NewTopic
+
+
+class Consumer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.stop_event = threading.Event()
+
+    def stop(self):
+        self.stop_event.set()
+
+    def run(self):
+        consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
+                                 auto_offset_reset='earliest',
+                                 group_id='mygroup2',
+                                 consumer_timeout_ms=1000)
+        consumer.subscribe(['my-topic'])
+
+        while not self.stop_event.is_set():
+            for message in consumer:
+                print(message)
+                if self.stop_event.is_set():
+                    break
+
+        consumer.close()
+
+
+def main():
+
+    tasks = [
+        # Producer(),
+        Consumer()
+    ]
+
+    # Start threads of a publisher/producer and a subscriber/consumer to 'my-topic' Kafka topic
+    for t in tasks:
+        t.start()
+
+    time.sleep(10)
+
+    # Stop threads
+    for task in tasks:
+        task.stop()
+
+    for task in tasks:
+        task.join()
+
+
+if __name__ == "__main__":
+    main()
